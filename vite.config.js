223d0eb6c3021308f0f970b9fd6c5a3662218d90
Krish3282 <kvkrishna1938@gmail.com>
2025-05-26 12:59:54 +0530

    to deploy it

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base :"/Healthcare-websire",
+  base :process.env.VITE_BASE-PATH || "/healthcare_dashboard",
 })
